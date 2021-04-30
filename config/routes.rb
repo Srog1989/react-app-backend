3aa3382acc2b42b2723964ef9ec7c8d17e7d60fc
Srog1989 <s.rogers0715@yahoo.com>
2021-04-29 21:05:26 -0600

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :favorites
+      resources :favorites, only: [:index, :create]
     end
-  end
-  
-  
-  
+  end 
 end
-
-# fetch(`http://localhost:3000/api/v1/favorites`)
 
